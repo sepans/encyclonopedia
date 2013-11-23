d7b25cf39568edf5c083d003e616cdb7efb2796c
Mani Nilchiani <mani.art@gmail.com>
2013-11-23 18:11:22 -0500

    Koshtam shepeshe shepeshkoshe sheshpaa raa
    Ghoori -e gol-ghermezi.

---
+++ chrome-extension/popup.js
@@ -0,0 +1,17 @@
+var popup = (function(w, d){
+
+    var init = function() {
+
+
+    };
+
+    return {
+        init: init
+    }
+
+})(window, document);
+
+document.addEventListener('DOMContentLoaded', function () {
+    popup.init();
+});
+
